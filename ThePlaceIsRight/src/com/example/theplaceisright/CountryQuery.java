285581bb3bddd28b95a81a5497ac970370065e52
sprakash0 <sonika.prakash@live.com>
2014-03-01 13:47:28 -0800

    Once again

--- ThePlaceIsRight/src/com/example/theplaceisright/CountryQuery.java
+++ ThePlaceIsRight/src/com/example/theplaceisright/CountryQuery.java
@@ -17,7 +17,7 @@ public class CountryQuery extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.country_query, menu);
 		return true;
 	}
 	
