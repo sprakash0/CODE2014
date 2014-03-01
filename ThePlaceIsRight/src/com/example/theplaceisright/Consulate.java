c9276705a4a9aec6949ea248899c505b0b538bbf
Kai Di Chen <kdchen115@gmail.com>
2014-02-28 22:10:34 -0800

    added consulate data structure

---
+++ ThePlaceIsRight/src/com/example/theplaceisright/Consulate.java
@@ -0,0 +1,99 @@
+package com.example.theplaceisright;
+
+public class Consulate {
+	private String country;
+	private Integer officeID;
+	private String city;
+	private String type;
+	private float latitude;
+	private float longitude;
+	private String address;
+	private String telephone;
+	private String emergency;
+	private String fax;
+	private String email;
+	private Boolean isPrimary;
+	private Boolean hasPassport;
+	
+	
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public Integer getOfficeID() {
+		return officeID;
+	}
+	public void setOfficeID(Integer officeID) {
+		this.officeID = officeID;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public float getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+	public float getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getEmergency() {
+		return emergency;
+	}
+	public void setEmergency(String emergency) {
+		this.emergency = emergency;
+	}
+	public String getFax() {
+		return fax;
+	}
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Boolean getIsPrimary() {
+		return isPrimary;
+	}
+	public void setIsPrimary(Boolean isPrimary) {
+		this.isPrimary = isPrimary;
+	}
+	public Boolean getHasPassport() {
+		return hasPassport;
+	}
+	public void setHasPassport(Boolean hasPassport) {
+		this.hasPassport = hasPassport;
+	}
+	
+	
+}
