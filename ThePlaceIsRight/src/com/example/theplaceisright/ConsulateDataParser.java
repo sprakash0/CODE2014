d615a01ed5c7ccb2a3a8bd1396736997b8dd36c1
unknown <jenni.hantula@gmail.com>
2014-03-01 12:34:11 -0800

    not much new

--- ThePlaceIsRight/src/com/example/theplaceisright/ConsulateDataParser.java
+++ ThePlaceIsRight/src/com/example/theplaceisright/ConsulateDataParser.java
@@ -2,7 +2,7 @@ package com.example.theplaceisright;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
+import java.util.ArrayList;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -10,50 +10,101 @@ import org.json.simple.JSONValue;
 
 public class ConsulateDataParser {
 
+	private String dataFile = "C:/Users/jenni/workspace/embassies-consulates-list.json";
+	
+	// For testing purposes; can be removed later
 	public static void main(String[] args) {
-
+		ConsulateDataParser parser = new ConsulateDataParser();
+		ArrayList<Consulate> result = parser.readData();
+		for (Consulate c : result) {
+			System.out.println("countryCode: " + c.getCountryCode());
+			System.out.println("country: " + c.getCountry());
+			System.out.println("officeID: " + c.getOfficeID());
+			System.out.println("city: " + c.getCity());
+			System.out.println("type: " + c.getType());
+			System.out.println("latitude: " + c.getLatitude());
+			System.out.println("longitude: " + c.getLongitude());
+			System.out.println("address: " + c.getAddress());
+			System.out.println("telephone: " + c.getTelephone());
+			System.out.println("fax: " + c.getFax());
+			System.out.println("isPrimary: " + c.getIsPrimary());
+			System.out.println("hassPassport: " + c.getHasPassport());
+		}
+	}	
+	
+	public ArrayList<Consulate> readData() {
+	
 		try {
-
-			Object obj = JSONValue.parse(new FileReader("C:/Users/jenni/workspace/embassies-consulates-list.json"));
+			
+			Object obj = JSONValue.parse(new FileReader(dataFile));
 
 			JSONObject jsonObject = (JSONObject) obj;
 
-			//			System.out.println(obj);
-			System.out.println(jsonObject.keySet());
-
 			JSONObject data = (JSONObject) jsonObject.get("data");
+			int size = (int) data.keySet().size();
+			
+			ArrayList<Consulate> consulates = new ArrayList<Consulate>(size);
 
 			for (Object n : data.keySet()) {
+				
+				Consulate consulate = new Consulate(); 
 				
 				JSONObject node = (JSONObject) data.get(n);
 				JSONObject country = (JSONObject) node.get("country");
 				JSONObject eng = (JSONObject) country.get("eng");
 				
-				String countryName = (String) eng.get("name");
-//				System.out.println(countryName);
-				
+				consulate.setCountry((String) eng.get("name"));
+				consulate.setCountryCode((String) country.get("country-iso"));
+
 				JSONArray offices = (JSONArray) node.get("offices");
 				
 				for (int i = 0; i < offices.size(); i++) {
 					
 					JSONObject office = (JSONObject) offices.get(i);
 					
-					if (!((String) office.get("lat")).equals("")) {
-						Double latitude = Double.valueOf((String) office.get("lat"));
-					}
-					if (!((String) office.get("lng")).equals("")) {
-						Double longitude = Double.valueOf((String) office.get("lng"));
-					}
+					consulate.setOfficeID(((Long) office.get("office-id")).intValue()); 	
 					
-				}
-				
+					boolean isPrimary = ((Long) office.get("is-primary") != 0)? true : false; 
+					consulate.setIsPrimary(isPrimary);
+					
+					boolean hasPassport = ((Long) office.get("has-passport-services") != 0)? true : false;
+					consulate.setHasPassport(hasPassport);
+					
+					// Set latitude and longitude if available; otherwise use min value of float. 
+					Float latitude = (!((String) office.get("lat")).equals(""))? Float.valueOf((String) office.get("lat")) : Float.MIN_VALUE;  
+					consulate.setLatitude(latitude);
+					
+					Float longitude = (!((String) office.get("lng")).equals(""))? Float.valueOf((String) office.get("lng")) : Float.MIN_VALUE;  
+					consulate.setLongitude(longitude);
+					
+					JSONObject embassy = (JSONObject) office.get("eng");
+					
+					// Empty strings OK; indicate null value.
+					consulate.setCity((String) embassy.get("city"));
+					consulate.setType((String) embassy.get("type"));
+					consulate.setAddress((String) embassy.get("address"));
+					consulate.setTelephone((String) embassy.get("tel-legacy"));
+					consulate.setFax((String) embassy.get("fax-legacy"));
+					consulate.setEmail((String) embassy.get("email-1"));
+					consulate.setEmergency((String) embassy.get("emergency-toll-free-legacy"));
+					
+					consulates.add(consulate);
+				}				
 			}
+			
+		return consulates;	
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
+		
+		return null;
 
 	}
+	
+	public void setFile(String fileName) {
+		
+		dataFile = fileName;
+	}
+	
 }
