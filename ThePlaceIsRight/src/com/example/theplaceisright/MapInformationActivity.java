0b732e4313196237b47217e690515b481333b870
amanda <akt_215@hotmail.com>
2014-03-01 19:08:16 -0800

    map information activity

--- ThePlaceIsRight/src/com/example/theplaceisright/MapInformationActivity.java
+++ ThePlaceIsRight/src/com/example/theplaceisright/MapInformationActivity.java
@@ -2,13 +2,18 @@ package com.example.theplaceisright;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapInformationActivity extends Activity {
 	
@@ -19,13 +24,31 @@ public class MapInformationActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_information);
         
-        // Get a handle to the Map Fragment
-        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        // Check that Google Play Services is available
+        if (GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext()) == ConnectionResult.SUCCESS) {
         
-        TextView consulateTextView = (TextView)findViewById(R.id.consulateTextView);
-        consulateTextView.setText("information about consulate");
+        	// Get a handle to the Map Fragment
+        	map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        	map.setMyLocationEnabled(true);
+        	
+        	// Set marker at current location
+            //LatLng locationLL = new LatLng(location.getLatitude(), location.getLongitude());
+            //map.addMarker(new MarkerOptions().position(locationLL));
+        
+        	// Set markers at consulate locations
+        	// set latitude and longitude of consulate
+        	LatLng consulateLL = new LatLng(0, 0);
+        	map.addMarker(new MarkerOptions().position(consulateLL));
+        
+        	// Text information
+        	TextView advisoryTextView = (TextView)findViewById(R.id.advisoryTextView);
+        	advisoryTextView.setText("show advisory");
+        	
+        	TextView consulateTextView = (TextView)findViewById(R.id.consulateTextView);
+        	consulateTextView.setText("information about consulate");
+      
+        }
     }
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
