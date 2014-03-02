f7735a9a9731fd9c50a2431a596da76986e99653
Kai Di Chen <kdchen115@gmail.com>
2014-03-01 21:42:09 -0800

    added advisory getter

---
+++ ThePlaceIsRight/src/com/example/theplaceisright/AdvisoryHTMLGetter.java
@@ -0,0 +1,12 @@
+package com.example.theplaceisright;
+
+public class AdvisoryHTMLGetter extends HTMLGetter{
+
+	  @Override
+	  protected void onPostExecute(String result) {
+	   // execution of result of Long time consuming operation
+		  //AdvisoryDataParser.readData(result);
+	  }
+	
+	
+}
